65ea5c96397a052486ea7d7cee125c17643cab63
yr6wdzkd69-ctrl <yr6wdzkd69@privaterelay.appleid.com>
2026-01-09 10:22:37 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -4,13 +4,12 @@
 #import <string.h>
 
 // --- Configuration ---
-#define OFF_PLAYER_UPDATE  0x1ADB974
+// These are the offsets causing the crash. We will test without using them first.
+#define OFF_PLAYER_UPDATE  0x1ADB974 
 #define OFF_IS_ADS         0x200EC04
 
 // --- Globals ---
 uint64_t unity_base = 0;
-void (*old_PlayerUpdate)(void *instance);
-bool (*Game_IsADSAiming)(void* player);
 
 // --- Utils ---
 intptr_t get_unity_slide() {
@@ -24,46 +23,36 @@ intptr_t get_unity_slide() {
     return 0;
 }
 
-// --- Hooks ---
-void new_PlayerUpdate(void *instance) {
-    if (old_PlayerUpdate) {
-        old_PlayerUpdate(instance);
-    }
-
-    if (instance == NULL || Game_IsADSAiming == NULL) {
-        return;
-    }
-
-    bool isScoped = Game_IsADSAiming(instance);
-    
-    if (isScoped) {
-        // Aimbot logic will be placed here
-    }
-}
-
 // --- Constructor ---
 __attribute__((constructor)) static void initialize() {
+    
+    // Wait 10 seconds to ensure game is loaded
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         unity_base = get_unity_slide();
         
         if (unity_base != 0) {
-            uint64_t addr_Update = unity_base + OFF_PLAYER_UPDATE;
-            uint64_t addr_IsADS  = unity_base + OFF_IS_ADS;
             
-            Game_IsADSAiming = (bool (*)(void*))(addr_IsADS);
-
-            MSHookFunction((void *)addr_Update, (void *)new_PlayerUpdate, (void **)&old_PlayerUpdate);
+            // --- CRASH FIX ---
+            // I have disabled MSHookFunction.
+            // If the game opens now without crash, it means your Offsets were wrong.
             
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ScopeBot" 
-                                                                           message:@"Active & Safe" 
+            // MSHookFunction(...);  <-- DISABLED
+            // MSHookFunction(...);  <-- DISABLED
+            
+            NSString *msg = [NSString stringWithFormat:@"Unity Found at: 0x%llx\nHooks are DISABLED to prevent crash.\nYou need correct offsets.", unity_base];
+            
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ScopeBot Diagnostic" 
+                                                                           message:msg
                                                                     preferredStyle:UIAlertControllerStyleAlert];
-            [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Understood" style:UIAlertActionStyleDefault handler:nil]];
             
             UIWindow *w = [[UIApplication sharedApplication] keyWindow];
             if (w && w.rootViewController) {
                 [w.rootViewController presentViewController:alert animated:YES completion:nil];
             }
+        } else {
+             NSLog(@"[ScopeBot] UnityFramework not found.");
         }
     });
 }
